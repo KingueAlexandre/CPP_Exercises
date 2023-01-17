47e91181530aa343b3ba7daefc92f6894bdabb02
KINGUE DIFFOUM LOBE ALEXANDRE <kinguediffouml@heckel02.u-pem.fr>
2023-01-17 16:17:30 +0100

    TP2. fin du cours.

--- src/translator.cpp
+++ src/translator.cpp
@@ -1,11 +1,56 @@
 #include <iostream>
+#include <set>
+#include <map>
+
+std::set<std::string> make_exit_commands()
+{
+    std::set<std::string> res;
+    res.emplace("q");
+    res.emplace("quit");
+    res.emplace("e");
+    res.emplace("end");
+    return res;
+}
 
 int main()
 {
+    std::string var;
+    std::set<std::string> setstop = make_exit_commands();
+    std::map<std::string, std::string> maptranslate;
+    std::string w1;
+    std::string w2;
     while (true)
     {
-        std::cout << "This is a translator, but it does nothing for now..." << std::endl;
-        std::cin.ignore();
+        std::cin >> var;
+        if (var == "add")
+        {
+
+            std::cin >> w1;
+            std::cin >> w2;
+            std::cout << w1 << " => " << w2 << std::endl;
+            maptranslate.insert({w1, w2});
+        }
+
+        if (var == "translate")
+        {
+            std::cin >> w1;
+            if (maptranslate.count(w1) > 0)
+            {
+                auto tmp = maptranslate.find(w1);
+                std::cout << tmp->second << std::endl;
+            }
+            else
+            {
+                std::cout << "???" << std::endl;
+            }
+        }
+
+        // std::getline(std::cin, var);
+
+        if (setstop.count(var))
+        {
+            break;
+        }
     }
 
     return 0;
