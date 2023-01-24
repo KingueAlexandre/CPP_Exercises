bc60b644158d21bf064c8c9e69c99b8957bf044f
KINGUE DIFFOUM LOBE ALEXANDRE <kinguediffouml@heckel02.u-pem.fr>
2023-01-24 16:03:34 +0100

    TP3. E - Magasin de peluches. (Test 9)

--- src/Plush.hpp
+++ src/Plush.hpp
@@ -1,1 +1,22 @@
 #pragma once
+
+class Plush
+{
+public:
+    Plush (const int& cost): _cost { cost }{
+    }
+
+    Plush (){
+        _cost = 10;
+    }
+
+    int get_cost() const{
+        return _cost;
+    }
+    void set_cost(int new_cost){
+        _cost = new_cost;
+    }
+private:
+    int _cost = 10;
+
+};
--- src/PlushStore.hpp
+++ src/PlushStore.hpp
@@ -1,1 +1,50 @@
 #pragma once
+#include <string>
+
+
+class PlushStore
+{
+public:
+    PlushStore (const std::string& name): _name { name }{
+    }
+
+    PlushStore (){
+        _name = "";
+    }
+
+    std::string get_name() const{
+        return _name;
+    }
+    void set_name(std::string new_name){
+        _name = new_name;
+    }
+    int get_wealth_amount() const{
+        return _wealth_amount;
+    }
+    int get_stock_size() const{
+        return _stock_size;
+    }
+    void loan(int wealth){
+        _wealth_amount += wealth;
+    }
+    int get_debt_amount(){
+        return _wealth_amount+_wealth_amount/10;
+    }
+    void make_plush(int plush){
+        if (_wealth_amount == 0){
+            return;
+        }else if(_wealth_amount-plush < 0){
+            _stock_size+=1;
+            _wealth_amount=0;
+        }else {
+            _stock_size+=1;
+            _wealth_amount-=plush;
+        }
+    }
+
+private:
+    std::string _name;
+    int _wealth_amount = 0;
+    int _stock_size = 0;
+
+};
