ad3a63a1a7ceadb03c973deaa72f9055df06d81b
KINGUE DIFFOUM LOBE ALEXANDRE <kinguediffouml@heckel02.u-pem.fr>
2023-02-16 16:32:55 +0100

    TP5. Fait en tp

--- src/Card.hpp
+++ src/Card.hpp
@@ -1,1 +1,78 @@
 #pragma once
+
+#include <string>
+
+enum CardType
+{
+    None,
+    Monster,
+    Spell,
+    Trap
+};
+
+std::string
+to_string(CardType card)
+{
+    switch (card)
+    {
+    case None:
+        return "None";
+    case Trap:
+        return "Trap";
+    case Spell:
+        return "Spell";
+    case Monster:
+        return "Monster";
+    default:
+        return "";
+    }
+}
+
+
+
+class Card
+{
+public:
+    Card(std::string id, CardType card) : _id{id}, _type{card}
+    {
+        _name = "";
+        _description = "";
+    }
+
+    std::string get_id() const
+    {
+        return _id;
+    }
+
+    CardType get_type() const
+    {
+        return _type;
+    }
+
+    std::string get_name() const
+    {
+        return _name;
+    }
+
+    std::string get_description() const
+    {
+        return _description;
+    }
+
+    void set_name(std::string name)
+    {
+        _name = name;
+    }
+
+    void set_description(std::string description)
+    {
+        _description = description;
+    }
+
+private:
+    std::string _id;
+    CardType _type;
+    std::string _name;
+    std::string _description;
+};
+
--- src/Monster.hpp
+++ src/Monster.hpp
@@ -1,1 +1,56 @@
 #pragma once
+
+#include <string>
+
+#include <Card.hpp>
+
+enum Attribute
+{
+    Dark,
+    Divine,
+    Earth,
+    Fire,
+    Light,
+    Water,
+    Wind
+};
+
+std::string
+to_symbol(Attribute attr)
+{
+    switch (attr)
+    {
+    case Dark:
+        return u8"闇";
+    case Divine:
+        return u8"神";
+    case Earth:
+        return u8"地";
+    case Fire:
+        return u8"炎";
+    case Light:
+        return u8"光";
+    case Water:
+        return u8"水";
+    case Wind:
+        return u8"風";
+    default:
+        return "";
+    }
+}
+
+class Monster
+{
+public:
+    Monster(std::string id, std::string name, Attribute attr, std::string monster_type, int _atk, int _def) : _card{Card{id, Monster}}, _attribute{attr}, _atk{atk}, _def{def}, _monster_type{monster_type}
+    {
+        _card.set_name(name);
+    }
+
+private:
+    Card _card;
+    Attribute _attribute;
+    int _atk;
+    int _def;
+    std::string _monster_type;
+};
