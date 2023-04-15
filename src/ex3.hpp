7ecb512f1433cda9516217e5c6d9c818244d4f5d
Alexandre Kingué <alexandrekingue@gmail.com>
2023-04-16 00:14:11 +0200

    Révision

--- src/ex3.hpp
+++ src/ex3.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <iterator>
+#include <type_traits>
+
 template <unsigned i>
 constexpr unsigned fibonacci()
 {
@@ -7,4 +10,48 @@ constexpr unsigned fibonacci()
         return 1;
     else
         return fibonacci<i - 1>() + fibonacci<i - 2>();
+}
+
+template <typename T1, typename T2>
+constexpr bool content_of_same_type()
+{
+    return std::is_same<typename T1::value_type, typename T2::value_type>::value;
+}
+
+template <typename T1, typename T2>
+void add_all(T1& t1, const T2& t2)
+{
+    if constexpr (content_of_same_type<T1, T2>())
+    {
+        for (auto it = t2.begin(); it != t2.end(); it++)
+        {
+            t1.emplace_back(*it);
+        }
+    }
+}
+
+template <typename T>
+constexpr bool has_random_access()
+{
+    return std::is_same<std::random_access_iterator_tag,
+                        typename std::iterator_traits<typename T::iterator>::iterator_category>::value;
+}
+
+template <typename T>
+int access(T const& container, unsigned i)
+{
+    if constexpr (has_random_access<T>() == true)
+    {
+        return container[i];
+    }
+    else
+    {
+        auto it = container.begin();
+        while (i != 0 && it != container.end())
+        {
+            it++;
+            i -= 1;
+        }
+        return *it;
+    }
 }
--- src/uge_unique_ptr.hpp
+++ src/uge_unique_ptr.hpp
@@ -1,7 +1,60 @@
 #pragma once
+#include <memory>
 
-using namespace uge
+namespace uge {
+template <typename T>
+class unique_ptr
 {
+public:
+    unique_ptr() { _ptr = nullptr; }
 
-    // Put your code for class unique_ptr here !
-}
+    unique_ptr(T* init)
+        : _ptr { init }
+    {
+        init = nullptr;
+    }
+
+    unique_ptr(const unique_ptr<T>& other)
+    {
+        if (other.get() != nullptr)
+            _ptr = new T { *(other._ptr) };
+        else
+            _ptr = nullptr;
+    }
+
+    unique_ptr(unique_ptr<T>&& other)
+        : _ptr { other._ptr }
+    {
+        other._ptr = nullptr;
+    }
+
+    unique_ptr<T>& operator=(uge::unique_ptr<T>&& other)
+    {
+        if (this != &other)
+        {
+            if (_ptr != nullptr)
+                delete _ptr;
+            _ptr       = other._ptr;
+            other._ptr = nullptr;
+        }
+        return (*this);
+    }
+
+    T& operator*() { return *_ptr; }
+
+    T* get() const { return _ptr; }
+
+    ~unique_ptr()
+    {
+        if (_ptr != nullptr)
+        {
+            delete _ptr;
+        }
+    }
+
+private:
+    T* _ptr;
+};
+
+// Put your code for class unique_ptr here !
+} // namespace uge
